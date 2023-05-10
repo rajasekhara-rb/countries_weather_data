ca44a222273bf5d8699e639ff989511d4ee97656
Budda Rajasekhara Reddy <rajasekharabudda009@gmail.com>
2023-05-10 23:12:38 +0530

    updated the getweatherData function

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ body.appendChild(container);
 
 // function for fetching the countries data from rest countries API.
 function displayCountriesDetails(obj, index) {
+    index = 0;
     let count = 1;
     obj.forEach(element => {
         element.id = count;
         let cardsContainer = document.querySelector("#cardsContainer");
 
         let card = document.createElement('div');
-        card.setAttribute('id', 'card');
+        card.setAttribute('id', `card-${index}`);
         card.setAttribute('class', 'card m-1 bg-light p-2 w-25');
         cardsContainer.appendChild(card);
 
@@ -53,8 +54,9 @@ function displayCountriesDetails(obj, index) {
         latlng.innerText = `id: ${element.id} lat: ${lat} lng: ${lng}`;
         cardBody.appendChild(latlng);
 
-        cardBtn.setAttribute('onclick', `getWeatherData("${element.id}", "${element.latlng[0]}", "${element.latlng[1]}")`);
+        cardBtn.setAttribute('onclick', `getWeatherData("${index}", "${element.latlng[0]}", "${element.latlng[1]}")`);
         count++;
+        index++;
     });
 }
 
@@ -74,23 +76,37 @@ async function getCountriesData() {
 getCountriesData();
 
 // function to get the weather data based on lat and lng 
-async function getWeatherData(id, lat, lng) {
+async function getWeatherData(index, lat, lng) {
     // console.log("clicked");
-
-    console.log(id, lat, lng);
+    // console.log(id, lat, lng);
+    // console.log(index);
     try {
         let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=5f02420cc75d106d910a910ee7ab7c04`)
-        let weather = response.json();
-        console.log(weather);
-        // let card = document.querySelector('#card');
-        // let WeatherContainer = document.createElement('div');
-        // WeatherContainer.setAttribute('class', 'card-body');
-        // card[id].appendChild(WeatherContainer);
+        let weather = await response.json();
+        let tempInC = ((weather.main.temp) - 273.15).toFixed(1);
+        let icon = weather.weather[0].icon;
+        // console.log(icon);
 
-        // let temp = document.createElement('h2');
-        // WeatherContainer.appendChild(temp);
-        // temp.innerText = `${weather.main.temp}`;
+        let iconimg = document.createElement("img");
+        iconimg.setAttribute("src", `https://openweathermap.org/img/wn/${icon}@2x.png`);
+        iconimg.setAttribute("class", "weather-icon");
+        // iconimg.style.border="1px solid grey";
+        iconimg.style.borderRadius="50%";
+        
+        // console.log(tempInC);
+        // console.log(index);
+        let card = document.querySelectorAll(".card");
 
+
+        let WeatherContainer = document.createElement('div');
+        WeatherContainer.setAttribute('class', 'card-body');
+        card[index].appendChild(WeatherContainer);
+
+        let temp = document.createElement('h6');
+        WeatherContainer.appendChild(temp);
+        temp.innerHTML = `Temperature: ${tempInC} &#x2103;`;
+
+        WeatherContainer.appendChild(iconimg);
     } catch (error) {
         console.log(error);
     }
